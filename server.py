4fed1ac02336b604e2114d8015a900ba7580a4bc
Luvkitri <janjedrzejewski@gmail.com>
2020-08-26 15:02:42 +0200

    Moved server methods to server class, plus some small changes.

--- server.py
+++ server.py
@@ -2,53 +2,60 @@ import asyncio
 import websockets
 import json
 import uuid
-from game import Player
+from game import Player, Game
 
 PLAYERS = dict()
 
-async def on_connect(data, websocket):
-    if len(PLAYERS) < 4:
-        await register_player(data)
-        # Welcome message is sent here
-        await websocket.send("welcome_message")
-    else:
-        print("Session is full")
 
-async def register_player(data):
-    # Create a player object
-    PLAYERS[uuid.uuid4()] = Player(data["nick"])
-    message = "player_pos"
-    await notify_players(message)
+class Server():
+    def __init__(self):
+        self.game = self.create_game()
 
-async def notify_players(message):
-    if PLAYERS:
-        await asyncio.wait([player.send(message) for player in PLAYERS])
+    async def on_connect(self, data, websocket):
+        if len(PLAYERS) < 4:
+            # Register player
+            client_uid = uuid.uuid4()
+            PLAYERS[client_uid] = Player(data['nick'], *self.game.possible_player_pos.pop())
 
-async def create_game():
-    pass
+            # Send starting position to each player            
+            await self.notify_players(PLAYERS[client_uid].get_pos())
 
-async def server(websocket, path):
-    create_game()
-
-    async for message in websocket:
-        data = json.loads(message)
-        if data["msg_code"] == "connect":
-            # Add new player
-            await on_connect(data, websocket)
-            print(f'Number of players: {len(PLAYERS)}')
-        elif data["msg_code"] == "player_move":
-            pass
-        elif data["msg_code"] == "player_plant_bomb":
-            pass
-        elif data["msg_code"] == "disconnect":
-            # Remove player
-            pass
+            # Welcome message is sent here
+            await websocket.send('welcome_message')
         else:
-            print("Unknown message.")
+            print("Session is full")
+
+    async def notify_players(self, message):
+        if PLAYERS:
+            await asyncio.wait([player.send(message) for player in PLAYERS])
+
+    async def create_game(self):
+        with open('json/config.json') as f:
+            config = json.load(f)
+        
+        return Game(config['map_size_x'], config['map_size_y'], config['box_number'], config['gift_number'])
+        
+
+    async def server(self, websocket, path):
+        async for message in websocket:
+            data = json.loads(message)
+            if data['msg_code'] == 'connect':
+                # Add new player
+                await self.on_connect(data, websocket)
+                print(f'Number of players: {len(PLAYERS)}')
+            elif data['msg_code'] == 'player_move':
+                pass
+            elif data['msg_code'] == 'player_plant_bomb':
+                pass
+            elif data['msg_code'] == 'disconnect':
+                # Remove player
+                pass
+            else:
+                print("Unknown message.")
 
         
 
-
+server = Server()
 start_server = websockets.serve(server, "localhost", 5000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
