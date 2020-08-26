b72de4dd4aa035d4f01a71a5ce13a9d579620350
Luvkitri <janjedrzejewski@gmail.com>
2020-08-26 15:03:49 +0200

    Created a game class.

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import json
+import random
+
 class Player():
     def __init__(self, nick:str, x:int, y:int):
         self.nick = nick
@@ -7,3 +10,45 @@ class Player():
     def set_player_pos(self, x:int, y:int):
         self.x = x
         self.y = y
+
+    def get_pos(self):
+        pos_message = {
+            "message_code": "player_pos",
+            "nick": self.nick,
+            "x": self.x,
+            "y": self.y
+        }
+
+        return json.dumps(pos_message)
+
+class Game():
+    def __init__(self, map_size_x:int, map_size_y:int, box_number:int, gift_number:int):
+        self.map_size_x = map_size_x
+        self.map_size_y = map_size_y
+        self.box_number = box_number
+        self.gift_number = gift_number
+        self.possible_player_pos = [(map_size_x/2, 0), (0, map_size_y/2), (map_size_x, map_size_y/2), (map_size_x/2, map_size_y)]
+        self.box = self.generate_boxes()
+
+    def generate_boxes(self):
+        boxes = []
+        for _ in range(self.box_number):
+            while True:
+                if pos := (random.randrange(0, self.map_size_x), random.randrange(0, self.map_size_y)) not in self.possible_player_pos:
+                    boxes.append(list(pos))
+                    break
+
+        return boxes
+
+
+    def create_welcome_msg(self, nick:str, uid, bombs_amount:int):
+        welcome_message = {
+            "msg_code": "welcome_msg",
+            "map_size_x": self.map_size_x,
+            "map_size_y": self.map_size_y,
+            "client_uid": uid,
+            "bombs_amount": bombs_amount,
+            "current_score": 0,
+            "box": self.box
+        }
+
